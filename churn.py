c2d0cf30df431062acec75f2989a8824c2e1d3f9
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2023-01-02 11:40:40 +0530

    nextt commit

--- churn.py
+++ churn.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
